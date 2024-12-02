95dca59fd911d15b7b9b92bc8f85b680b6a6dac4
musamaya <muhammadsandimaulana07@gmail.com>
2024-12-02 08:48:59 +0700

    perbaikan jawaban task 3

--- task3.js
+++ task3.js
@@ -51,20 +51,89 @@ const getAllData = () =>{
     return new Promise((resolve, reject) =>{
         setTimeout(() => {
             if(data.length != 0) {
-                return resolve(data)
+                 resolve(data)
             }else{
-                return reject(new Error("data tidak tersedia"))
+                 reject(new Error("data tidak tersedia"))
             }
         }, 3000);
     })
 }
 
-getAllData().then((value) => console.log(value)).catch((err) => console.log(err))
+// getAllData().then((value) => console.log(value)).catch((err) => console.log(err))
 
 const  filterValue = async () =>{
-
+    try {
+        let totalNilai =0
+        const response = await getAllData()
+        const filtered = []
+        let lulus = false
+        
+        if(response){
+            for(let i = 0; i < response.length; i++){
+                for(const mataKuliah in response[i].nilai){
+                    totalNilai += response[i].nilai[mataKuliah]
+                }
+              
+                lulus = (totalNilai / Object.keys(response[i].nilai).length) >= 75 ? true : false
+                response[i] = {...response[i], lulus}
+                totalNilai = 0
+                if(response[i].lulus){
+                  filtered.push(response[i])
+                }
+            }
+            
+            return filtered
+        } else{
+            throw Error("Nilai tidak tersedia")
+        }
+       
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+filterValue()
+
+// 
+
+const nilai = [80,90,73,87,45]
+
+const getAverage = (nilai) =>{
+    return new Promise((resolve, reject) =>{
+        let average = 0
+        if(!nilai){
+            reject(new Error("nilai anda tidak tersedia"))
+        } else{
+             nilai.forEach((item) =>{
+             average += item
+
+        })   
+        resolve(average / nilai.length)
+        }
+            
+    })
+}
+
+ getAverage(nilai).then((average) => average > 80? console.log("Selamat anda lulus") :  console.log("Maaf anda belum lulus") ).catch((error)=> console.log(error) )
+
+const getAverageAsyn = async (nilai) =>{
+    try {
+        const average = await getAverage(nilai)
+        if(average > 80){
+            return "Selamat anda lulus"
+        }else{
+            return "Maaf anda belum lulus"
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+// fetch data
 const URL = "https://jsonplaceholder.typicode.com/users"
 const fetchData = async (url) => {
     try {
