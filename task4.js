f931e153ee844f9d5d39562f230727ce92bf566a
musamaya <muhammadsandimaulana07@gmail.com>
2024-12-03 08:01:28 +0700

    update  convert to  lowercase

--- task4.js
+++ task4.js
@@ -1,22 +1,25 @@
 // palindrom
 
-const palindrom  = (text) =>{
-    const reversed = text.split('').reverse().join('')
-    if(reversed === text){
-        return `${text} adalah palindrom;`
-    }else{
-        return `${text} bukan palindrom;`
-
+const palindrom = (sentences) => {
+  if (typeof sentences === 'string') {
+    const temp = sentences.toLocaleLowerCase()
+    const reversed = temp.split('').reverse().join('')
+    if (reversed === temp) {
+      return `${sentences} adalah palindrom`
+    } else {
+      return `${sentences} bukan palindrom`
     }
+  }
 }
 
-console.log(palindrom('malam'));
+console.log(palindrom('malam'))
+console.log(palindrom('kasur rusak'))
+console.log(palindrom('Kasur Rusak'))
 
 // reverse words
-const reverseWord  = (text) =>{
-    const reversed = text.split(' ').reverse().join(' ')
-    return reversed
-    
+const reverseWord = (text) => {
+  const reversed = text.split(' ').reverse().join(' ')
+  return reversed
 }
 
-console.log(reverseWord("saya belajar javascript "));
+console.log(reverseWord('saya belajar javascript'))
